c48a974d4a7adcc3c84c971b81c566391f427ae0
mvpnitesh <parimalanitesh@gmail.com>
2016-04-28 20:35:19 +0530

    Two Urls

--- myplugin/content/mypanel/urls.py
+++ myplugin/content/mypanel/urls.py
@@ -4,4 +4,5 @@ from myplugin.content.mypanel import views
 
 urlpatterns = [
     url(r'^nitesh/', views.IndexView.as_view(), name='index'),
+    url(r'^hello/', views.IndexView.hello_view(), name='index'),
 ]
--- myplugin/content/mypanel/views.py
+++ myplugin/content/mypanel/views.py
@@ -3,3 +3,6 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = 'identity/mypanel/index.html'
+
+    def hello(generic.TemplateView):
+        template_name = 'identity/mypanel/hello.html'
