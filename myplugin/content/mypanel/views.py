622cedd187f6c5c941fe87ca5ed4fbb6f6e50b6e
mvpnitesh <parimalanitesh@gmail.com>
2016-04-28 21:00:05 +0530

    Removed the 2nd views

--- myplugin/content/mypanel/views.py
+++ myplugin/content/mypanel/views.py
@@ -3,6 +3,3 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = 'identity/mypanel/index.html'
-
-    def hello(generic.TemplateView):
-        template_name = 'identity/mypanel/hello.html'
